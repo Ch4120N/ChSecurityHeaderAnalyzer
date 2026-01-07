a94acfe3bad8553494ae6c6fde47eedcdfa5547a
Ch4120N <charonsecagency@gmail.com>
2026-01-06 21:59:52 -0800

    add required lib

---
+++ modules/scanner.py
@@ -0,0 +1,11 @@
+"""
+URL Scanner Module
+"""
+
+import requests
+import time
+from typing import Dict, Optional, Tuple
+from urllib.parse import urlparse
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
