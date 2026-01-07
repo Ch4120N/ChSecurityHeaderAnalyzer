571274bb5ea6f67297b6cee42cff93e253e4c432
Ch4120N <charonsecagency@gmail.com>
2026-01-06 22:03:21 -0800

    add HeaderAnalyzer class and __init__  method

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -7,3 +7,11 @@ from typing import Dict, List, Tuple, Any
 from datetime import datetime
 
 
+class HeaderAnalyzer:
+    def __init__(self, config: Dict):
+        self.config = config
+        self.required_headers = config['security_headers']['required']
+        self.recommended_headers = config['security_headers']['recommended']
+        self.vulnerable_headers = config['security_headers']['vulnerable_headers']
+    
+    
