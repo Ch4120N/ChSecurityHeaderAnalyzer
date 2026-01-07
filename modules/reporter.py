610a1a3cda41a836bc4bbad065ef629b1f746b5a
Ch4120N <charonsecagency@gmail.com>
2026-01-06 22:08:02 -0800

    add ReportGenerator class and __init__ method

--- modules/reporter.py
+++ modules/reporter.py
@@ -11,3 +11,11 @@ import pandas as pd
 from jinja2 import Template
 
 
+class ReportGenerator:
+    def __init__(self, config: Dict):
+        self.config = config
+        self.output_dir = config['reporting']['default_output_dir']
+        
+        # Create output directory if it doesn't exist
+        os.makedirs(self.output_dir, exist_ok=True)
+
