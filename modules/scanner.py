63334ba41fe4baf75b2705e06055f4125a58fa6f
Ch4120N <charonsecagency@gmail.com>
2026-01-06 22:01:28 -0800

    add _create_session method

--- modules/scanner.py
+++ modules/scanner.py
@@ -15,5 +15,37 @@ class SecurityScanner:
     def __init__(self, config: Dict):
         self.config = config
         self.session = self._create_session()
-    
+        
+    def _create_session(self) -> requests.Session:
+        """Create a configured requests session"""
+        session = requests.Session()
+        
+        # Configure retry strategy
+        retry_strategy = Retry(
+            total=3,
+            backoff_factor=1,
+            status_forcelist=[429, 500, 502, 503, 504]
+        )
+        
+        adapter = HTTPAdapter(
+            max_retries=retry_strategy,
+            pool_connections=100,
+            pool_maxsize=100
+        )
+        
+        session.mount("http://", adapter)
+        session.mount("https://", adapter)
+        
+        # Set default headers
+        session.headers.update({
+            'User-Agent': self.config['scanner']['user_agent'],
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'Connection': 'close',
+            'Upgrade-Insecure-Requests': '1'
+        })
+        
+        return session
+
     
