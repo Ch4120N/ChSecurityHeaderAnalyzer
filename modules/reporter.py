b27445b0f4739b5e165b8951fd5a141f7ee4e5d7
Ch4120N <charonsecagency@gmail.com>
2026-01-06 22:07:42 -0800

    add required lib

---
+++ modules/reporter.py
@@ -0,0 +1,13 @@
+"""
+Report Generator Module
+"""
+
+import json
+import csv
+import os
+from typing import Dict, List, Any
+from datetime import datetime
+import pandas as pd
+from jinja2 import Template
+
+
