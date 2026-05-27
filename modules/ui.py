a9bcb3897dd7adc8b6a71866604e49b76f89b030
Ch4120N <charonsecagecy@gmail.com>
2026-05-28 01:52:27 +0430

    Updating banner

--- modules/ui.py
+++ modules/ui.py
@@ -21,17 +21,18 @@ class ConsoleUI:
         self.console = Console()
 
     def display_header(self):
-        centered_banner = Align.center(r"""[bold red]
+        banner = r"""[bold red]
    ___ _     __                                _            _               _                    
   / __\ |__ / _\ ___  ___  /\  /\___  __ _  __| | ___ _ __ /_\  _ __   __ _| |_   _ _______ _ __ 
  / /  | '_ \\ \ / _ \/ __|/ /_/ / _ \/ _` |/ _` |/ _ \ '__//_\\| '_ \ / _` | | | | |_  / _ \ '__|
 / /___| | | |\ \  __/ (__/ __  /  __/ (_| | (_| |  __/ | /  _  \ | | | (_| | | |_| |/ /  __/ |   
-\____/|_| |_\__/\___|\___\/ /_/ \___|\__,_|\__,_|\___|_| \_/ \_/_| |_|\__,_|_|\__, /___\___|_|   
+\____/|_| |_\__/\___|\___\/ /_/ \___|\__,_|\__,_|\___|_| \_/ \_/_| |_|\__,_|_|\__, /___\___|_| [bold white] v2.0 [/bold white] 
                                                                               |___/              [/bold red]
-        """)
-        self.console.print(centered_banner)
-        self.console.print("[bold white]Owned By [/][bold blue][[/] [bold red]Ch4120N[/] [bold blue]][/]", justify="center")
-        self.console.print("[bold blue]GitHub.com/Ch4120N[/]", justify='center')
+                                   [bold white]Owned By [/][bold blue][[/] [bold red]Ch4120N[/] [bold blue]][/]
+                                    [bold blue]GitHub.com/Ch4120N[/]"""
+        print('-'*100)
+        self.console.print(banner)
+        print('-'*100, '\n\n')
 
     def print_info(self, message):
         self.console.print(f"[bold cyan][[/] [white]*[/] [bold cyan]][/] [white]{message}[/]")
